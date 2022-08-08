8092a783b1fb9d5a79d74f39f1bb9b4b6d08c4ea
juhalo <107361221+juhalo@users.noreply.github.com>
2022-08-08 23:34:14 +0300

    Change :hover colors

--- main.js
+++ main.js
@@ -17,8 +17,3 @@ function divide(a, b) {
 function operate(numOne, numTwo, functionName) {
   return functionName(numOne, numTwo);
 }
-
-// console.log(operate(2, 3, add));
-// console.log(operate(2, 3, subtract));
-// console.log(operate(2, 3, multiply));
-// console.log(operate(2, 3, divide));
