367320ad4e7fe310570b195ec0c9d68946438255
juhalo <107361221+juhalo@users.noreply.github.com>
2022-08-09 11:25:50 +0300

    Add EventListener for numbers
    Adds addEventListener that changes operandOne (screen textContent)
    when there is no selected operator. This makes sure that the zero
    before the number is removed.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let operandOne;
+let operandTwo;
+let operator;
+const screen = document.querySelector('.screen');
+console.log(screen);
+
 function add(a, b) {
   return a + b;
 }
@@ -17,3 +23,23 @@ function divide(a, b) {
 function operate(numOne, numTwo, functionName) {
   return functionName(numOne, numTwo);
 }
+
+function main() {
+  operandOne = 0;
+  screen.textContent = operandOne;
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button) => button.addEventListener('click', clickBtn));
+}
+
+function clickBtn(e) {
+  if (e.target.classList.contains('btn-number')) {
+    if (!operator) {
+      operandOne = +screen.textContent; // '+' to remove whitespace
+      operandOne += e.target.textContent;
+      operandOne = +operandOne; // '+' to remove '0' if first character
+      screen.textContent = operandOne;
+    }
+  }
+}
+
+main();
